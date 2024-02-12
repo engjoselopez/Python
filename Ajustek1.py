31f54d45f45daa6ff8683050d2ed3ed9277be892
engjoselopez <129974823+engjoselopez@users.noreply.github.com>
2024-02-11 20:48:11 -0400

    Add files via upload

--- Ajustek1.py
+++ Ajustek1.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
-# %matplotlib inline
+%matplotlib inline
 
 data_filename= 'Advertising.csv'
 df=pd.read_csv(data_filename)
@@ -41,7 +41,6 @@ def find_nearest(array,value):
     return idx, array[idx]
 
 x=np.linspace(np.min(x_true),np.max(x_true))
-# print(x)
 y=np.zeros(len(x))
 
 for i, xi in enumerate(x):
@@ -57,10 +56,38 @@ plt.title("Gasto en Publicidad por TV vs Retorno en Ventas")
 # =============================================================================
 # 
 # =============================================================================
-
-
-
-x=df['TV']
+x=df[['TV']]
 y=df['Sales']
 
-x_train, x_test, y_train, y_test = train_test_split()
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.4,train_size=0.6,random_state=42)
+##why random.state=42
+k_value_min=1
+k_value_max=70
+k_list=np.linspace(k_value_min,k_value_max,70)
+fig, ax=plt.subplots(figsize=(10,6))
+
+j=0
+
+for k_value in k_list:
+    
+    model=KNeighborsRegressor(n_neighbors=int(k_value))
+    model.fit(x_train,y_train)
+    y_pred=model.predict(x_test)
+    colors=['grey','r','b']
+    
+    if k_value in [1,10,70]:
+        xvals=np.linspace(x.min(),x.max(),100)
+        ypreds=model.predict(xvals)
+        ax.plot(xvals,ypreds,'-',label=f'k={int(k_value)}',linewidth=j+2,color=colors[j])
+        j+=1
+        
+ax.legend(loc='lower right',fontsize=20)
+ax.plot(x_train, y_train, 'x',label='train',color='k')
+ax.set_xlabel('TV budget in $1000',fontsize=20)
+ax.set_ylabel('Sales in $1000',fontsize=20)
+plt.tight_layout()
+
+model=KNeighborsRegressor(n_neighbors=int(1))
+print(model)
+test=model.predict([[200]])
+print(test)
